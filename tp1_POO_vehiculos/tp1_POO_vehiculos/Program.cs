e56bd046e180b25a794b0bae818e02dbc4170971
EnzoEzequiel <enzo.ep@hotmail.com>
2021-09-09 00:47:48 -0300

    Se sube codigo de tp1
    se sube codigo junto con los archivos de proyecto y solucion de tp1

---
+++ tp1_POO_vehiculos/tp1_POO_vehiculos/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp1_POO_vehiculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tipoVehiculo = "nada";
+
+            Taxi taxisito;
+            Omnibus ominisito;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Favor de Ingresar el tipo de vehiculo (taxi u omnibus) o 'listo' para finalizar: ");
+                    tipoVehiculo = Console.ReadLine();
+
+                    if (tipoVehiculo.ToLower() == "taxi")
+                    {
+                        Console.WriteLine("Ahora favor de ingresar la cantidad de pasajeros: ");
+                        taxisito = new Taxi(int.Parse(Console.ReadLine()));
+
+                    }
+                    else
+                    {
+                        if (tipoVehiculo.ToLower() == "omnibus")
+                        {
+                            Console.WriteLine("Ahora favor de ingresar la cantidad de pasajeros: ");
+                            ominisito = new ominisito(int.Parse(Console.ReadLine()));
+                        }
+                        else
+                        {
+                        }
+                    }
+
+                    /*
+                    switch (tipoVehiculo)
+                    {
+                        case "taxi":
+                            Console.WriteLine("Ahora favor de ingresar la cantidad de pasajeros: ");
+                            pasajerosTaxi =int.Parse(Console.ReadLine());
+                          
+                            
+                            Console.ReadKey();
+                            Console.Clear();
+                           
+                            break;
+
+                        case "omnibus":
+                            Console.WriteLine("Ahora favor de ingresar la cantidad de pasajeros: ");
+                            pasajerosOmnibus = int.Parse(Console.ReadLine());
+                            Console.ReadKey();
+                            Console.Clear();
+                           
+                            break;
+                        default:
+                            Console.WriteLine("Ingrese tipo de vehiculo (taxi u omnibus). ");
+                            Console.ReadKey();
+                            Console.Clear();
+                         
+                            break;
+                    }*/
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Dato erroneo, intentelo nuevamente.");
+                    Console.WriteLine("Presione cualquier tecla para regresar al menu");
+                    Console.ReadKey();
+                    Console.Clear();
+
+                }
+            } while (tipoVehiculo != "listo");
+
+            Console.WriteLine("Ejecucion finalizada.");
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
+}
