d905cb066a0e3898018c409355862f9921a9f5fb
EnzoEzequiel <enzo.ep@hotmail.com>
2021-09-09 03:13:53 -0300

    Se sube codigo TP1 Vehiculos
    Se sube codigo junto con los archivos de programa y solucion de tp1

--- tp1_POO_vehiculos/tp1_POO_vehiculos/Program.cs
+++ tp1_POO_vehiculos/tp1_POO_vehiculos/Program.cs
@@ -10,56 +10,91 @@ namespace tp1_POO_vehiculos
     {
         static void Main(string[] args)
         {
-            string tipoVehiculo = "nada";
             int ingresos = 0;
-            Taxi taxisito;
-            Omnibus ominisito;
+            string respuesta = "si";
+            Taxi taxi;
+            Omnibus omnibus;
 
             List<TransportePublico> listaVehiculos = new List<TransportePublico>();
 
-            do
+            while (respuesta != "no" && ingresos < 5)
             {
-            
-                Console.WriteLine("Favor de Ingresar el tipo de vehiculo (taxi u omnibus) o 'listo' para finalizar: ");
-                tipoVehiculo = Console.ReadLine();
-
-                if (tipoVehiculo.ToLower() == "taxi")
+                ingresos++;
+                Console.WriteLine("favor de ingresar la cantidad de pasajeros del omnibus {0}: ",ingresos);
+                try
                 {
-                    ingresos++;
-                    Console.WriteLine("Ahora favor de ingresar la cantidad de pasajeros: ");
-                    taxisito = new Taxi(int.Parse(Console.ReadLine()));
-                    listaVehiculos.Add(taxisito);
+                    
+                    omnibus = new Omnibus(int.Parse(Console.ReadLine()));
+                    while (omnibus.Pasajeros > 100 || omnibus.Pasajeros < 0)
+                    {
+                        Console.WriteLine("un Omnibus no puede llevar mas de 100 personas ni se pueden ingresar numeros negativos");
+                        Console.WriteLine("Ahora favor de ingresar la cantidad de pasajeros del omnibus {0}: ", ingresos);
+                        omnibus = new Omnibus(int.Parse(Console.ReadLine()));
+                    }
 
+                    listaVehiculos.Add(omnibus);
+                    
+                    Console.WriteLine("Ingresado con exito!!");
+                    Console.ReadKey(); 
+                    Console.Clear();
+                    
                 }
-                else
+                catch (FormatException)
                 {
-                    if (tipoVehiculo.ToLower() == "omnibus")
-                    {
-                        ingresos++;
-                        Console.WriteLine("Ahora favor de ingresar la cantidad de pasajeros: ");
-                        ominisito = new Omnibus(int.Parse(Console.ReadLine()));
-                        listaVehiculos.Add(ominisito);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ingreso erroneo, favor de reintentar ingresar el tipo de vehiculo (taxi u omnibus) o 'listo' para finalizar:");
-                        tipoVehiculo = Console.ReadLine();
-                    }
+                    Console.WriteLine("dato ingresado erroneo, desea continuar?  (si/no): ");
+                    respuesta = Console.ReadLine();
+                    ingresos--;
                 }
- 
-                
-            } while (tipoVehiculo != "listo" && ingresos<10);
-
-            Console.WriteLine("los vehiculos son: ");
-            for (int i = 0; i < listaVehiculos.Count; i++)
+            }
+            while (respuesta != "no" && ingresos < 10)
             {
-                Console.WriteLine("{0} {1}= {2}", listaVehiculos[i].ToString(), i + 1, listaVehiculos[i].Pasajeros);
+
+                ingresos++;
+                Console.WriteLine("Ahora favor de ingresar la cantidad de pasajeros del taxi {0}: ",(ingresos-5));
+                try
+                {
+                    
+                    taxi = new Taxi(int.Parse(Console.ReadLine()));
+                    while (taxi.Pasajeros > 4 || taxi.Pasajeros < 0)
+                    {
+                        Console.WriteLine("un taxi no puede llevar mas de 4 personas ni se pueden ingresar numeros negativos");
+                        Console.WriteLine("Ahora favor de ingresar la cantidad de pasajeros del taxi {0}: ", (ingresos - 5));
+                        taxi = new Taxi(int.Parse(Console.ReadLine()));
+                    }
+
+                    listaVehiculos.Add(taxi);
+                    
+                    Console.WriteLine("Ingresado con exito!!");
+                    Console.ReadKey();
+                    Console.Clear();
+                    
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("dato ingresado erroneo, desea continuar?  (si/no): ");
+                    respuesta = Console.ReadLine();
+                    ingresos--;
+                }
             }
 
-            Console.WriteLine("Ejecucion finalizada con "+ingresos+" ingresos");
 
-            
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Los vehiculos son: ");
+            Console.WriteLine(" ");
+            for (int i = 0; i < listaVehiculos.Count; i++)
+            {
+                Console.WriteLine("{0} {1}: {2}", listaVehiculos[i].ToString(), i + 1, listaVehiculos[i].Pasajeros);
+            }
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Ejecucion finalizada con " + ingresos + " vehiculos");
+
+
             Console.ReadKey();
+
+
+
 
         }
 
