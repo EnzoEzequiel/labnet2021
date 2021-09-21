63133845e394fcd94d5b7b3155b926ec4ce4abe8
EnzoEzequiel <enzo.ep@hotmail.com>
2021-09-21 16:05:11 -0300

    Se entrega tp4_EF
    Se entrega codigo con archivo de proyecto y solucion de tp4.lab.EF

---
+++ tp4_EF.Lab/Logic.EFTests/SuppliersLogicTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.EF;
+
+namespace Logic.EF.Tests
+{
+    [TestClass()]
+    public class SuppliersLogicTests:BaseLogic
+    {
+        [TestMethod]
+        public void EncontrarTest()
+        {
+            //Arrange
+            int supplier = 30;
+            Suppliers resultado;
+
+            //Act
+            resultado=context.Suppliers.Find(supplier);
+
+            //Assert
+            Assert.AreEqual(resultado, null);
+
+        }
+    }
+}
