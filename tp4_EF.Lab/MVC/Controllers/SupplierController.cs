80ee77bf8bb636beb6d72e7daf3c4947c2dc5f5a
EnzoEzequiel <enzo.ep@hotmail.com>
2021-09-28 12:17:51 -0300

    Segundop commit de prueba tp7
    se sube codigo de prueba para tp7

---
+++ tp4_EF.Lab/MVC/Controllers/SupplierController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entities.EF;
+using Logic.EF;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class SupplierController : Controller
+    {
+        SuppliersLogic logic = new SuppliersLogic();
+        // GET: Supplier
+        public ActionResult Index()
+        {
+            
+            List<Entities.EF.Suppliers> suppliers = logic.GetAll();
+
+            List<SupplierView> supplierView = suppliers.Select(s => new SupplierView
+            {
+                Id = s.SupplierID,
+                NombreSupplier = s.ContactName,
+
+            }).ToList();
+            return View(supplierView);
+        }
+
+        public ActionResult Insert()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Insert(SupplierView supplierView)
+        {
+            try
+            {
+                Suppliers supplierEntity = new Suppliers
+                {
+                    
+                    ContactName = supplierView.NombreSupplier
+                    
+                };
+
+                logic.Add(supplierEntity);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            logic.Delete(id);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ tp4_EF.Lab/MVC/Models/SupplierView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class SupplierView
+    {
+        public int Id { set; get; }
+
+        public string NombreSupplier { set; get; }
+    }
+}
